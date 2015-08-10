f02fb4eacebd9a91df7bb991b075dcd3fca1b7e1
Robert <R.Treharne@liverpool.ac.uk>
2015-08-10 20:00:26 +0100

    getting average data (done for eff so far)

--- projects/views_conflict-20150810-164546.py
+++ projects/views_conflict-20150810-164546.py
@@ -9,8 +9,20 @@ from profiles.forms import UserProfile
 from upload.models import Cell
 import json
 from django.core.serializers.json import DjangoJSONEncoder
-import time
-from datetime import datetime
+
+def get_average(cells):
+    average = []
+    av = 0
+    for i in range(len(cells)-1):
+        n = 1
+        average.append(cells[0][0])
+        if cells[4][i] == cells[4][i+1]:
+            n += 1
+            av = (average[len(average)-1] + cells[0][i+1])/n
+        else:
+            average.append(av)
+            n=1
+    return average
 
 @login_required
 def new_project(request):
@@ -33,36 +45,14 @@ def new_project(request):
 	
     return render(request, 'new_project.html', {'project_form': project_form, 'submitted': submitted})
 
-def get_stats(data):
-    build = []
-    elist = []
-    stats = []
-    delta = 1.0
-    current = time.mktime(data[0].date.timetuple())
-    for dat in data:
-        build.append([float(dat.eff), time.mktime(dat.date.timetuple())])
-        if build[-1][1] > current - delta:
-            elist.append(build[-1][0])
-            continue
-
-        else:
-            stats.append([datetime.fromtimestamp(current).strftime("%Y-%m-%dT%H:%M:%S"), elist])
-            current = build[-1][1]
-            elist = [build[-1][0]]
-
-    #stats_json = json.dumps(list(stats), cls=DjangoJSONEncoder)
-    return stats 
-
 def project(request, project_id=1):
     project = Project.objects.get(id=project_id)
     cells = Cell.objects.filter(project=project).order_by('-date')
-    stats = get_stats(cells)
     data = Cell.objects.filter(project=project).values_list('eff', 'jsc', 'voc', 'ff',  'date', 'id')
     data_json = json.dumps(list(data), cls=DjangoJSONEncoder)
     dict = {'project': project,
             'cells': cells,
-            'data_json': data_json,
-            'stats': stats}
+            'data_json': data_json}
     return render(request, 'project.html', dict)
 
 def jv(request, project_id=1, cell_id=1):
